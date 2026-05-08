5bfacd464606d9ce8ca0aa7160888ca8af3eb5d8
pradeep <pradeepnigam9452@gmail.com>
2026-05-08 08:22:52 +0530

    app.js

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,29 @@
-import express from 'express';
-import cors from 'cors'
-import mongoose from 'mongoose';
-import ConnectDb from './utils/Db.js'
-import userRoutes from './routes/userRoutes.js';
-import weatherRoutes from './routes/WetherRoutes.js'
-import cityRoutes from './routes/CityRoutes.js'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import ConnectDb from "./utils/Db.js";
+import userRoutes from "./routes/userRoutes.js";
+import weatherRoutes from "./routes/WetherRoutes.js";
+import cityRoutes from "./routes/CityRoutes.js";
+
 const app = express();
-app.use(cors())
-app.use(express.json())
-ConnectDb()
 
-app.use('/api', userRoutes);
+app.use(cors());
+app.use(express.json());
+
+ConnectDb();
+
+app.use("/api", userRoutes);
 app.use("/api", weatherRoutes);
-app.use('/api',cityRoutes);
-app.get('/',(req,res)=>{
-    res.json({massage : 'hello '})
-})
+app.use("/api", cityRoutes);
 
+app.get("/", (req, res) => {
+  res.json({ message: "Hello" });
+});
 
-app.listen(8000)
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
