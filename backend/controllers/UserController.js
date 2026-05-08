6fbc203b1ef3d76ac16b9e7083deaad9fd34078d
pradeep <pradeepnigam9452@gmail.com>
2026-05-08 08:54:34 +0530

    fixed filename

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,6 +1,6 @@
 import User from '../models/User.js';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';4
+import jwt from 'jsonwebtoken';
 import generateToken from '../utils/function.js'
      
 const login = async (req, res) => {
@@ -31,34 +31,47 @@ const login = async (req, res) => {
 
 
 
-const register = async(req,res)=>{
-    try {
-        const {email,name,password}= req.body;
-        const isUser = await User.findOne({email});
-        if(isUser){
-            return res.status(404).json({message : "already a user "})
-        }
+const register = async (req, res) => {
+  try {
+    const { email, name, password } = req.body;
 
-        const hashpassword =await bcrypt.hash(password,10);
-        const newUser = await User.create({name,email,
-            password : hashpassword})
-
-        const token = generateToken(newUser)
-
-        return res.status(201).json({
-            message: "User registered successfully",
-            token,
-            user: {
-                id: newUser._id,
-                name: newUser.name,
-            }
-        });
-
-
-    } catch (error) {
-         console.error(error.message);
-        return res.status(500).json({ message: "Server error" });
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
     }
-}
 
+    const isUser = await User.findOne({ email });
+
+    if (isUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashpassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashpassword,
+    });
+
+    const token = generateToken(newUser);
+
+    return res.status(201).json({
+      message: "User registered successfully",
+      token,
+      user: {
+        id: newUser._id,
+        name: newUser.name,
+      },
+    });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
 export  {login, register}
